788042328ac7c55b029cdab473d93944063e3ef1
DataCherub <140235829+lmozafari89@users.noreply.github.com>
2024-05-01 12:53:54 +0900

    Update AuctionContract.sol
    In this version:
    
    Access control modifiers are added to restrict certain functions to the contract owner.
    Error messages are provided to users for better feedback.
    Gas optimization techniques and comprehensive testing are not implemented here but should be considered during further development and deployment.

--- AuctionContract.sol
+++ AuctionContract.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract Auction {
+    address public owner;
     address public highestBidder;
     uint256 public highestBid;
     bool public ended;
@@ -9,8 +10,26 @@ contract Auction {
     event BidPlaced(address indexed bidder, uint256 amount);
     event AuctionEnded(address winner, uint256 highestBid);
 
-    function bid() public payable {
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    modifier onlyBeforeEnd() {
         require(!ended, "Auction already ended");
+        _;
+    }
+
+    modifier onlyAfterEnd() {
+        require(ended, "Auction not yet ended");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function bid() public payable onlyBeforeEnd {
         require(msg.value > highestBid, "Bid must be higher than the current highest bid");
 
         if (highestBidder != address(0)) {
@@ -23,17 +42,14 @@ contract Auction {
         emit BidPlaced(msg.sender, msg.value);
     }
 
-    function withdraw() public {
+    function withdraw() public onlyAfterEnd {
         require(msg.sender != highestBidder, "Highest bidder cannot withdraw");
-        require(ended, "Auction not yet ended");
 
         uint256 amount = address(this).balance;
         payable(msg.sender).transfer(amount);
     }
 
-    function auctionEnd() public {
-        require(!ended, "Auction already ended");
-
+    function auctionEnd() public onlyOwner onlyBeforeEnd {
         ended = true;
         emit AuctionEnded(highestBidder, highestBid);
     }
